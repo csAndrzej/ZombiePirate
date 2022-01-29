32d58458ef31e632c7204916b1385f0c425a488c
csAndrzej <andrzejd.kolodziej@gmail.com>
2022-01-29 08:49:17 +0000

    Movement script changes
    Factored in mouse position and used spherical interpolation to make the movement script a lot smoother.

--- Assets/Scripts/PlayerController2D.cs
+++ Assets/Scripts/PlayerController2D.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 public class PlayerController2D : MonoBehaviour
 {
-    private Vector2 moveDirection;
+    private Vector2 mouseInput;
+    private Vector2 movementInput;
     private Vector2 currentVelocity;
     private Rigidbody2D p_RigidBody;
     
@@ -16,24 +17,26 @@ public class PlayerController2D : MonoBehaviour
 
     void Update()
     {
-        moveDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        mouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        movementInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         currentVelocity = p_RigidBody.velocity;
     }
 
     private void FixedUpdate()
     {
-        if (moveDirection.x != 0)
+        Vector3 MouseWorldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // This is required as the Input.mousePosition gives coordinates relative to the screen rather than our world
+
+        float targetAngle = -Mathf.Atan2(MouseWorldPoint.x, MouseWorldPoint.y) * (180 / Mathf.PI);
+        transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.Euler (0, 0, targetAngle), rotationSpeed * Time.deltaTime);
+        // Used spherical interpolation for 'smoother' rotation of the player 
+
+        if (movementInput != Vector2.zero)
         {
-            p_RigidBody.rotation -= moveDirection.x * rotationSpeed;
-        }
-        if (moveDirection.y != 0)
-        {
-            currentVelocity.y = Mathf.Cos(p_RigidBody.rotation * (Mathf.PI / 180)) * moveDirection.y;
-            currentVelocity.x = -Mathf.Sin(p_RigidBody.rotation * (Mathf.PI / 180)) * moveDirection.y;
+            currentVelocity.y = Mathf.Cos(p_RigidBody.rotation * (Mathf.PI / 180)) * movementInput.y + Mathf.Sin(p_RigidBody.rotation * Mathf.PI / 180) * movementInput.x;
+            currentVelocity.x = -Mathf.Sin(p_RigidBody.rotation * (Mathf.PI / 180)) * movementInput.y + Mathf.Cos(p_RigidBody.rotation * Mathf.PI / 180) * movementInput.x;
 
             p_RigidBody.velocity += currentVelocity * moveSpeed;
         }
     }
-    
-
 }
