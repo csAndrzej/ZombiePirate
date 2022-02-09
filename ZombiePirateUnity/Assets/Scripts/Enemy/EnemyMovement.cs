504a9a834fa21b6c1fa70e450d521429686615a6
kazcon2001 <74197278+kazcon2001@users.noreply.github.com>
2022-02-09 17:40:55 +0000

    Removed unnecessary code

--- ZombiePirateUnity/Assets/Scripts/Enemy/EnemyMovement.cs
+++ ZombiePirateUnity/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -22,14 +22,11 @@ public class EnemyMovement : MonoBehaviour
     {
         if (!aiPath.reachedDestination)
         {
-            //if (!transform.parent.name.Contains("Super"))
-           // {
                 //Turn to path direction
                 direction = aiPath.desiredVelocity;
                 transform.right = direction;
-            //}
         }
-        else //if (!transform.parent.name.Contains("Super"))
+        else 
         {
             //Turn to player
             Vector3 targetDir = target.position - transform.position;
@@ -37,18 +34,6 @@ public class EnemyMovement : MonoBehaviour
             Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, q, rotationSpeed * Time.deltaTime);
 
-            Debug.Log("works as shoudl");
         }
-        //else
-        //{
-        //    //turn away from the player
-        //    Vector3 targetDir = transform.position - target.position;
-        //    float angle = Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg;
-        //    Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
-        //    transform.rotation = Quaternion.RotateTowards(transform.rotation, q, rotationSpeed * Time.deltaTime);
-
-        //    Debug.Log("doesnt work");
-        //}
-
     }
 }
